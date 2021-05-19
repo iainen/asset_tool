8921e3b788028382b5c09b6843a4e792cf7a6a29
zhongkaizhu <zhongkaizhu@tencent.com>
2021-05-19 10:35:26 +0800

    add more test case

--- excel/excel_test.go
+++ excel/excel_test.go
@@ -24,3 +24,39 @@ func TestName(t *testing.T) {
 		log.Printf("%#v", line)
 	}
 }
+
+type CheckLine struct {
+	AssetTag string `excel:"资产标签"`
+	Status string `excel:"状态"`
+	Location string `excel:"默认位置"`
+}
+
+func TestName2(t *testing.T) {
+	lines := make([]CheckLine, 0)
+	Load("../ct/G10.xlsx", &lines)
+	for _, line := range lines {
+		log.Printf("%#v", line)
+	}
+}
+
+func TestAll(t *testing.T) {
+	allMap := make(map[string]Line, 0)
+	all := make([]Line, 0)
+	Load("../ct/custom-assets-report-2021-05-18-051041.xlsx", &all)
+	for _, line := range all {
+		allMap[line.AssetTag] = line
+	}
+
+	toChecks := make([]CheckLine, 0)
+	Load("../ct/G10.xlsx", &toChecks)
+	for _, line := range toChecks {
+		if f, ok := allMap[line.AssetTag]; ok {
+			f.Location = line.Location
+			f.Status = line.Status
+
+			log.Printf("found: %#v", f)
+		} else {
+			log.Printf("not found: %#v", line)
+		}
+	}
+}
