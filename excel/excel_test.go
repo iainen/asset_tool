21cf07c627ede58d4ae8e06b7a30ea9616d6e3c7
zhongkaizhu <zhongkaizhu@tencent.com>
2021-05-19 11:07:31 +0800

    add gocsv test

--- excel/excel_test.go
+++ excel/excel_test.go
@@ -1,16 +1,18 @@
 package excel
 
 import (
+	"github.com/gocarina/gocsv"
 	"log"
+	"os"
 	"testing"
 )
 
 type Line struct {
 	Company string
-	AssetTag string `excel:"Asset Tag"`
-	Serial string `excel:"Serial Number"`
-	Model string `excel:"Model name"`
-	Brand string `excel:"Model Number"`
+	AssetTag string `excel:"Asset Tag" csv:"Asset Tag"`
+	Serial string `excel:"Serial Number" csv:"Serial Number"`
+	Model string `excel:"Model name" csv:"Model name"`
+	Brand string `excel:"Model Number" csv:"Model Number"`
 	Manufacturer string
 	Category string
 	Status string
@@ -18,7 +20,7 @@ type Line struct {
 }
 
 func TestName(t *testing.T) {
-	lines := make([]Line, 0)
+	lines := make([]*Line, 0)
 	Load("../ct/custom-assets-report-2021-05-18-051041.xlsx", &lines)
 	for _, line := range lines {
 		log.Printf("%#v", line)
@@ -32,7 +34,7 @@ type CheckLine struct {
 }
 
 func TestName2(t *testing.T) {
-	lines := make([]CheckLine, 0)
+	lines := make([]*CheckLine, 0)
 	Load("../ct/G10.xlsx", &lines)
 	for _, line := range lines {
 		log.Printf("%#v", line)
@@ -40,23 +42,39 @@ func TestName2(t *testing.T) {
 }
 
 func TestAll(t *testing.T) {
-	allMap := make(map[string]Line, 0)
-	all := make([]Line, 0)
+	allMap := make(map[string]*Line, 0)
+	all := make([]*Line, 0)
 	Load("../ct/custom-assets-report-2021-05-18-051041.xlsx", &all)
 	for _, line := range all {
 		allMap[line.AssetTag] = line
 	}
 
-	toChecks := make([]CheckLine, 0)
+	export := make([]*Line, 0)
+
+	toChecks := make([]*CheckLine, 0)
 	Load("../ct/G10.xlsx", &toChecks)
 	for _, line := range toChecks {
 		if f, ok := allMap[line.AssetTag]; ok {
 			f.Location = line.Location
 			f.Status = line.Status
 
-			log.Printf("found: %#v", f)
+			// log.Printf("found: %#v", f)
+			export = append(export, f)
 		} else {
 			log.Printf("not found: %#v", line)
 		}
+	}
+
+	outCsv, err := os.OpenFile("G10.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer outCsv.Close()
+
+	//csvContent, err := gocsv.MarshalString(&export)
+	//fmt.Println(csvContent) // Display all clients as CSV string
+	err = gocsv.MarshalFile(&export, outCsv)
+	if err != nil {
+		panic(err)
 	}
 }
